331a55d7302aa33ac2fb3b03ae9de510e2e3f555
Willian Maia <willian.maia@sysmap.com.br>
2024-04-22 16:31:49 -0300

    ajuste cadastro e header

--- src/app/components/cadastro/cadastro.component.ts
+++ src/app/components/cadastro/cadastro.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { JogadorService } from '../../services/jogador.service';
+import { Jogador } from '../../models/jogador.model';
 
 @Component({
   selector: 'app-cadastro',
@@ -8,22 +9,51 @@ import { JogadorService } from '../../services/jogador.service';
 })
 export class CadastroComponent {
 
-  jogador: any = {}; // Objeto para armazenar os dados do jogador
+  jogador: Jogador = { id: '', nome: '', sobrenome: '', dataNascimento: '' };
+  nomeInvalido = false;
+  sobrenomeInvalido = false;
+  dataNascimentoInvalida = false;
 
   constructor(private jogadorService: JogadorService) { }
 
   salvarJogador() {
-    // Chamar o serviço para salvar o jogador no JSON Server
-    this.jogadorService.salvarJogador(this.jogador).subscribe(
-      (response) => {
-        console.log('Jogador cadastrado com sucesso:', response);
-        // Limpar o formulário após o cadastro
-        this.jogador = {};
-      },
-      (error) => {
-        console.error('Erro ao cadastrar jogador:', error);
-      }
-    );
-  }
+    // Validar campos obrigatórios
+    if (!this.jogador.nome) {
+      this.nomeInvalido = true;
+    } else {
+      // Validar formato do nome utilizando regex (apenas letras e espaços, de 2 a 50 caracteres)
+      const nomeRegex = /^[a-zA-ZÀ-ÿ\s]{2,50}$/;
+      this.nomeInvalido = !nomeRegex.test(this.jogador.nome);
+    }
 
+    if (!this.jogador.sobrenome) {
+      this.sobrenomeInvalido = true;
+    } else {
+      // Validar formato do sobrenome utilizando regex (apenas letras e espaços, de 2 a 50 caracteres)
+      const sobrenomeRegex = /^[a-zA-ZÀ-ÿ\s]{2,50}$/;
+      this.sobrenomeInvalido = !sobrenomeRegex.test(this.jogador.sobrenome);
+    }
+
+    if (!this.jogador.dataNascimento) {
+      this.dataNascimentoInvalida = true;
+    } else {
+      // Validar formato da data de nascimento utilizando regex (dd/mm/aaaa)
+      const dataNascimentoRegex = /^\d{2}\/\d{2}\/\d{4}$/;
+      this.dataNascimentoInvalida = !dataNascimentoRegex.test(this.jogador.dataNascimento);
+    }
+
+    // Se todos os campos estão preenchidos e válidos, salvar o jogador
+    if (!this.nomeInvalido && !this.sobrenomeInvalido && !this.dataNascimentoInvalida) {
+      this.jogadorService.salvarJogador(this.jogador).subscribe(
+        (response) => {
+          console.log('Jogador cadastrado com sucesso:', response);
+          // Limpar o formulário após o cadastro
+          this.jogador = { id: '', nome: '', sobrenome: '', dataNascimento: '' };
+        },
+        (error) => {
+          console.error('Erro ao cadastrar jogador:', error);
+        }
+      );
+    }
+  }
 }
